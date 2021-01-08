42e4190151d7217e7a705a6d3c7bea345eb56ff7
Matias <matiasrh84@gmail.com>
2021-01-07 22:12:27 -0300

    primer commit

---
+++ src/test/java/com/matiasrh/proyecto/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasrh.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
